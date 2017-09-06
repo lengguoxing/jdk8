679c2c642cc5ebcad41d739f28a98ab6b6c3a1f4
lengguoxing <626826586@qq.com>
2017-09-06 15:00:18 +0800

    update to part01.Test11

---
+++ src/main/java/com/lgx/jdk8/part01/Test10Stream.java
@@ -0,0 +1,101 @@
+package com.lgx.jdk8.part01;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+
+/**
+ * 通过流和函数式编程的方法来完成查询过滤
+ */
+public class Test10Stream {
+    public static void main(String[] args) {
+        Person person1 = new Person("zhangsan", 20);
+        Person person2 = new Person("lisi", 25);
+        Person person3 = new Person("wangwu", 30);
+
+        List<Person> list = Arrays.asList(person1, person2, person3);
+
+        Test10Stream Test = new Test10Stream();
+        List<Person> resultList = Test.getPersonsByUsername("zhangsan", list);
+        resultList.forEach(person -> System.out.println("persons 1 = [" + person + "]"));
+
+        System.out.println("--------------------");
+
+        List<Person> resultList2 = Test.getPersonsByAge(25, list);
+        resultList2.forEach(person -> System.out.println("persons 2 = [" + person + "]"));
+
+        System.out.println("--------------------");
+
+        List<Person> resultList3 = Test.getPersonsByAge2(25, list, (ageOfPerson, personList) ->
+                personList.stream().filter(person -> person.getAge() >= ageOfPerson).collect(Collectors.toList()));
+        resultList3.forEach(person -> System.out.println("persons 3 = [" + person + "]"));
+    }
+
+    //通过name查询列表中符合条件的对象
+    public List<Person> getPersonsByUsername(String name, List<Person> persons){
+        //先转换成流，过滤流里面每一个对象，把名字符合我们要求的过滤出来，得到一个流，最后把这些流组合成一个集合
+        return persons.stream().filter(person -> person.getName().equals(name)).collect(Collectors.toList());
+    }
+
+    //通过age查询列表中符合条件的对象
+    public List<Person> getPersonsByAge(int age, List<Person> persons){
+        //先定义BiFunction
+        //三个参数分别是年龄，输入的用户集合，输出的用户集合结果
+        BiFunction<Integer, List<Person>, List<Person>> biFunction = (ageOfPerson, personList) -> {
+            return  personList.stream().filter(person -> person.getAge() >= ageOfPerson).collect(Collectors.toList());
+        };
+        //简写如下：
+        BiFunction<Integer, List<Person>, List<Person>> biFunction2 = (ageOfPerson, personList) ->
+              personList.stream().filter(person -> person.getAge() >= ageOfPerson).collect(Collectors.toList());
+
+
+        //再应用bifunction
+        return biFunction2.apply(age, persons);
+    }
+
+    //改进版
+    public List<Person> getPersonsByAge2(int age, List<Person> persons, BiFunction<Integer, List<Person>, List<Person>> biFunction){
+        //再应用bifunction
+        return biFunction.apply(age, persons);
+    }
+
+
+
+}
+
+
+
+class Person{
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ src/main/java/com/lgx/jdk8/part01/Test11Predicate.java
@@ -0,0 +1,91 @@
+package com.lgx.jdk8.part01;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * Predicate的讲解，在上一个例子已经用到了
+ */
+public class Test11Predicate {
+    public static void main(String[] args) {
+        Predicate<String> predicate = p -> p.length() > 5;
+
+        System.out.println("helloworld = [" + predicate.test("helloworld") + "]");
+
+
+        Test11Predicate test = new Test11Predicate();
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        test.conditionFilter(list, item -> item % 2 == 0); //获取偶数
+        test.conditionFilter(list, item -> item % 2 == 1); //获取奇数
+        test.conditionFilter(list, item -> item > 5);      //获取大于5的数
+        test.conditionFilter(list, item -> true);          //获取所有
+
+        System.out.println("-----------------------");
+
+        test.conditionAndFilter(list, item -> item % 2 == 0, item -> item > 5);       //获取大于5的偶数
+        test.conditionAndFilter(list, item -> item % 2 == 0, item -> item % 3 == 0);  //获取能同时被2,和3整除的
+        test.conditionOrFilter(list, item -> item % 2 == 0, item -> item % 3 == 0);   //获取能被2或者3整除的
+        test.conditionAndNegateFilter(list, item -> item % 2 == 0, item -> item % 3 == 0);   //获取不能同时被2,和3整除的
+
+        boolean b = test.isEqual("helloworld").test("helloworld");   //判断2个参数是否相等的
+        System.out.println("b = [" + b + "]");
+        boolean b2 = test.isEqual2(new Date()).test(new Date());   //判断2个参数是否相等的
+        System.out.println("b2 = [" + b2 + "]");  //张龙老是的电脑运行是false，我的是true???
+
+    }
+
+    /**
+     * 函数式编程有区别之前的面相对象编程：
+     * 1：面相对象传递是参数，方法里面做具体的处理
+     * 2：函数式编程传递是行为，由调用者来决定怎么处理
+     * @param list
+     * @param predicate
+     */
+    public void conditionFilter(List<Integer> list, Predicate<Integer> predicate){
+        for (Integer integer : list){
+            if(predicate.test(integer)){
+                System.out.print(integer + ", ");
+            }
+        }
+        System.out.println("");
+    }
+
+    public void conditionAndFilter(List<Integer> list, Predicate<Integer> predicate, Predicate<Integer> predicate2){
+        for (Integer integer : list){
+            if(predicate.and(predicate2).test(integer)){
+                System.out.print(integer + ", ");
+            }
+        }
+        System.out.println("");
+    }
+
+    public void conditionOrFilter(List<Integer> list, Predicate<Integer> predicate, Predicate<Integer> predicate2){
+        for (Integer integer : list){
+            if(predicate.or(predicate2).test(integer)){
+                System.out.print(integer + ", ");
+            }
+        }
+        System.out.println("");
+    }
+
+    public void conditionAndNegateFilter(List<Integer> list, Predicate<Integer> predicate, Predicate<Integer> predicate2){
+        for (Integer integer : list){
+            if(predicate.and(predicate2).negate().test(integer)){
+                System.out.print(integer + ", ");
+            }
+        }
+        System.out.println("");
+    }
+
+
+    public Predicate<String> isEqual(Object object){
+        return Predicate.isEqual(object);//判断2个参数是否相等的
+    }
+
+    public Predicate<Date> isEqual2(Object object){
+        return Predicate.isEqual(object);//判断2个参数是否相等的
+    }
+
+}
